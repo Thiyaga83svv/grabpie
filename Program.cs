f9f7f468ebff30b77f5932ab46d1c878043737f0
Thiyagarajan Ramu <zxtru1@wowcorp.com.au>
2021-06-11 07:20:50 +1000

    try catch block

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +14,14 @@ namespace GrabPie
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            try
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Host stopped unexpectedly", ex);
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
