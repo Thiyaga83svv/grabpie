aaabfcdd0e58fe684223631301adcd0d0a852ef5
Thiyagarajan Ramu <zxtru1@wowcorp.com.au>
2021-06-25 19:11:18 +1000

    Details page

--- Controller/PieController.cs
+++ Controller/PieController.cs
@@ -28,5 +28,15 @@ namespace GrabPie.Controllers
 
             return View(piesListViewModel);
         }
+
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+
+            if (pie == null)
+                return NotFound();
+
+            return View(pie);
+        }
     }
 }
